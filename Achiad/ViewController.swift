9385e77d0321c4cf2baa5daa21ddad1090d8a612
Rowland Gault <rowlandg@DevMac.local>
2017-01-14 13:00:50 +0000

    Initial Commit

---
+++ Achiad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Achiad
+//
+//  Created by Rowland Gault on 14/01/2017.
+//  Copyright © 2017 Rogue Application Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
